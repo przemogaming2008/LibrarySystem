5445ee9dafc927c8deb3943944ce892d89fda8e1
Wojciech Gudz <wojtek.gudz@interia.eu>
2026-02-11 13:21:56 +0100

    feat: add MenuUI

--- include/MenuUI.hpp
+++ include/MenuUI.hpp
@@ -1,1 +1,29 @@
 #pragma once
+#include "LibraryManager.hpp"
+
+class MenuUI {
+public:
+    MenuUI(LibraryManager& manager) : manager(manager) {}
+
+    void mainMenu();
+
+private:
+    LibraryManager& manager;
+
+    // handlers opcji 1-4
+    void handleAddBook();
+    void handleAddUser();
+    void handleListBooks();
+    void handleListUsers();
+
+    // helpers UI
+    void printHeader() const;
+    void printMenu() const;
+
+    int readInt(const char* prompt);
+    std::string readLine(const char* prompt);
+
+    // formatowanie
+    static void printBooksTableHeader();
+    static void printUsersTableHeader();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,28 @@
 
 int main(){
     std::setlocale(LC_ALL, "pl_PL.UTF-8");
+
+    // LibraryManager manager;
+
+    // Book book{1, "Hobbit", "Tolkien", 1937, "Allen & Unwin"};
+    // User user{1,"Jan","Kowalski"};
+
+    // manager.addBook(book);
+    // manager.addUser(user);
+
+    // std::cout << "is book borrowed: "<< book.isBorrowed() << std::endl;
     
+    // std::cout << "\n=== LISTA KSIAZEK ===\n";
+    // manager.listBooks();
+
+    // std::cout << "=== LISTA UZYTKOWNIKOW ===\n";
+    // manager.listUsers();
+
+    // return 0;
+
     LibraryManager manager;
-
-    Book book{1, "Hobbit", "Tolkien", 1937, "Allen & Unwin"};
-    User user{1,"Jan","Kowalski"};
-
-    manager.addBook(book);
-    manager.addUser(user);
-
-    std::cout << "is book borrowed: "<< book.isBorrowed() << std::endl;
-    
-    std::cout << "\n=== LISTA KSIAZEK ===\n";
-    manager.listBooks();
-
-    std::cout << "=== LISTA UZYTKOWNIKOW ===\n";
-    manager.listUsers();
-
+    MenuUI ui(manager);
+    ui.mainMenu();
     return 0;
 
 }
